b3e1cb345ea3f77d7ed053252543db0d2772964a
Andrew Miller <socrates1024@gmail.com>
2016-04-26 16:45:40 -0400

    Now works with recursive terms

--- hoas/ilc1.hs
+++ hoas/ilc1.hs
@@ -45,7 +45,7 @@ type LinVar repr (vid::Nat) a = forall (v::Nat) (i::[Maybe Nat]) (o::[Maybe Nat]
 type RegVar repr a = forall (v::Nat) (h::[Maybe Nat]) . repr v h h a
 
 -- to distinguish linear arrow from regular arrow
-newtype a :-<>: b = Lolli {unLolli :: a -> b}
+newtype a :-<>: b = Lolli {unLolli :: a -> IO b}
 data a :*: b = Tensor a b deriving Show
 data One = One
 type a :&: b = (a, b)
@@ -60,21 +60,21 @@ newtype Wr a = Wr {unWr :: Chan a }
 newtype Base a = Base { unBase :: a } deriving Show
 
 class Lin (repr :: Nat -> [Maybe Nat] -> [Maybe Nat] -> * -> *) where
---    llam :: (LinVar repr vid a -> repr (S vid) (Just vid ': hi) (Nothing ': ho) b) -> 
---            repr vid hi ho (a :-<>: b)
+    llam :: (LinVar repr vid a -> repr (S vid) (Just vid ': hi) (Nothing ': ho) b) -> repr vid hi ho (a :-<>: b)
     nu :: ((RegVar repr     (Wr a), {-- write end --}
             LinVar repr vid (Rd a)) -> {--  read end --}
            repr (S vid) (Just vid ': hi) (Nothing ': ho) b) -> repr vid hi ho b
     (|||) :: repr vid hi h a -> repr vid h ho b -> repr vid hi ho b
     base :: a -> repr vid h h (Base a)
-    app :: (a -> repr vid h h b) -> repr vid h h (Base a) -> repr vid h h b
+    app :: (a -> repr vid h h b) -> repr vid hi h (Base a) -> repr vid hi h b
     prt :: String -> repr vid h h ()
     stop :: repr vid h h ()
+    wrClosed :: repr vid hi ho (Wr a) -> repr Z '[] '[] a -> repr vid hi ho ()
     wr :: repr vid hi h (Wr a) -> repr vid h ho a -> repr vid hi ho ()
     rd :: repr vid hi ho (Rd a) -> repr vid hi ho (Rd a :*: a)
     lett :: repr vid hi h a -> (LinVar repr vid a -> repr vid h ho b) -> repr vid hi ho b
     drop :: repr vid hi h a -> repr vid h ho b -> repr vid hi ho b
---    (<^>) :: repr vid hi h (a :-<>: b) -> repr vid h ho a -> repr vid hi ho b
+    (<^>) :: repr vid hi h (a :-<>: b) -> repr vid h ho a -> repr vid hi ho b
 
     -- Pair
     (<**>) :: repr vid hi h a -> repr vid h ho b ->
@@ -102,8 +102,11 @@ defn x = x
 newtype R (vid::Nat) (hi::[Maybe Nat]) (ho::[Maybe Nat]) a = R {unR :: IO a}
 
 instance Lin R where
---  llam f = R $ Lolli $ \x -> unR $ f $ R x
---  f <^> x = R $ unLolli (unR f) (unR x)
+    llam f = R $ return $ Lolli $ \x -> unR $ f $ R (return x)
+    f <^> x = R $ do 
+                 f' <- unR f
+                 x' <- unR x
+                 unLolli f' x'
     nu f = R $ do
       x <- newChan;
       unR $ f (R $ return $ Wr x, R $ return $ Rd x)
@@ -119,6 +122,9 @@ instance Lin R where
     wr c x = R $ do
                c' <- unR c
                unR x >>= writeChan (unWr c')
+    wrClosed c x = R $ do
+                     c' <- unR c
+                     unR x >>= writeChan (unWr c')
     rd c = R $ do
              c' <- unR c
              x <- readChan (unRd c')
@@ -150,7 +156,29 @@ test5 = nu $ \(_,x) -> drop x stop ||| prt "hi"
 test6 = nu $ \(w,r) -> drop (wr w (base 1)) (wr w (base 2)) ||| 
         letStar (rd r) (\(r, a) -> 
                             letStar (rd r) (\(r, b) -> drop r (a <**> b)))
-        --                letStar (rd r) (\(r, b) -> drop r b))
+test7 = nu $ \(ww,rr) -> drop rr $
+        nu $ \(w,r) -> drop (wrClosed w $ base ww) (wrClosed w $ base ww) ||| 
+        letStar (rd r) (\(r, a) -> 
+                            letStar (rd r) (\(r, b) -> drop r (a <**> b)))
+test8 = app (\x -> base x) (base 1)
+
+readForever :: Defn (Rd a :-<>: ())
+readForever = defn $ llam $ \c -> letStar (rd c) $ \(c, x) -> drop x $ drop (readForever <^> c) stop
+
+readForever' :: Int -> Defn (Rd a :-<>: ())
+readForever' n = defn $ llam $ \c -> letStar (rd c) $ \(c, x) -> drop (prt (show n)) $ drop x $ drop (readForever' (n+1) <^> c) stop
+
+test9 = nu $ \(w,r) -> drop (wr w (base ())) (wr w (base ())) ||| readForever' 0 <^> r
+
+readForever'' :: Lin repr => Int -> Int -> repr vid hi h (Rd a) -> repr vid hi h b
+readForever'' m n c = letStar (rd c) $ \(c, x) -> drop x $ drop (prt ("[" ++ show m ++ "]:" ++ show n)) $ readForever'' m (n+1) c
+
+test10 = nu $ \(w,r) -> drop (wr w (base ())) (wr w (base ())) ||| readForever'' 0 0 r
+
+multiplex :: Lin repr => repr vid hi h (Rd (Base Int)) -> repr vid hi h (Base Int)
+multiplex c = letStar (rd c) $ \(c, x) -> drop c $ app (\x -> base x) x
+
+
 -- good = defn $ llam $ \f -> llam $ \x -> f <^> x
 
 main = do

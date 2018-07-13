b56abf5a2c21057d73b6cf4a1f8df6b8a1a4b925
kev-liao <kliao6@illinois.edu>
2018-07-13 10:32:55 -0500

    Inference stuff

--- haskell-ilc/src/Infer.hs
+++ haskell-ilc/src/Infer.hs
@@ -1,19 +1,41 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-} -- Look up what this means.
+
 module Infer where
+
+import Prelude hiding (foldr)
 
 import Control.Monad.State
 import Control.Monad.Except
+import Data.Monoid
+import Data.List (nub)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import Syntax
 import Type
 
-type Infer a = ExceptT TypeError (State Unique a)
+newtype TypeEnv = TypeEnv (Map.Map Name Scheme)
+    deriving Monoid
+
+data Unique = Unique { count :: Int }
+
+type Infer = ExceptT TypeError (State Unique)
+type Subst = Map.Map TVar Ty
+
+data TypeError
+    = UnificationFail Ty Ty
+    | InfiniteType TVar Ty
+    | UnboundVariable Name
+
+{-extend :: TypeEnv -> (Name, Scheme) -> TypeEnv
+extend (TypeEnv env) (x, s) = TypeEnv $ Map.insert x s env
 
 runInfer :: Infer (Subst, Ty) -> Either TypeError Scheme
 runInfer m = case evalState (runExceptT m) initUnique of
     Left err -> Left err
     Right res -> Right $ closeOver res
 
-type Subst = Map.Map TVar Ty
+
 
 nullSubst :: Subst
 nullSubst = Map.empty
@@ -56,3 +78,65 @@ fresh = do
     s <- get
     put s{count = count s + 1}
     return $ TVar $ TV (letters !! count s)
+
+occursCheck :: Substitutable a => TVar -> a -> Bool
+occursCheck a t = a `Set.member` ftv t
+
+unify :: Ty -> Ty -> Infer Subst
+unify (TCon a) (TCon b) | a == b = return nullSubst -- ^ Uni-Const?
+unify (TVar a) t = bind a t -- ^ Uni-VarLeft
+unify t (Tvar a) = bind a t -- ^ Uni-VarRight
+unify (l `TArr` r) (l' `TArr` r') = do -- ^ Uni-Arrow
+    s1 <- unify l l'
+    s2 <- unify (apply s1 r) (apply s1 r')
+    return (s2 `compose` s1)
+
+unify t1 t2 = throwError $ UnificationFail t1 t2
+
+
+bind :: TVar -> Ty -> Infer Subst
+bind a t | t == TVar a     = return nullSubst -- ^ Uni-Var
+         | occursCheck a t = throwError $ InfiniteType a t
+         | otherwise       = return $ Map.singleton a t
+
+instantiate :: Scheme -> Infer Ty -- ^ T-Inst
+instantiate (Forall as t) = do
+    as' <- mapM (const fresh) as
+    let s = Map.fromList $ zip as as'
+    return $ apply s t
+
+generalize :: TypeEnv -> Ty -> Scheme -- ^ T-Gen
+generalize env t = Forall as t
+    where as = Set.toList $ ftv t `Set.difference` ftv env
+
+infer :: TypeEnv -> Expr -> Infer (Subst, Ty)
+infer env ex = case ex of
+    EVar x -> lookupEnv env x -- ^ T-Var
+    -- TODO: Unit?
+    ELam (PVar x) e -> do  -- ^ T-Lam
+        tv <- fresh
+        let env' = env `extend` (x, Forall [] tv)
+        (s1, t1) <- infer env' e
+        return (s1, apply s1 tv `TArr` t1)
+    EApp e1 e2 -> do
+        tv <- fresh
+        (s1, t1) <- infer env e1
+        (s2, t2) <- infer (apply s1 env) e2
+        s3       <- unify (apply s2 t1) (TArr t2 tv)
+        return (s3 `compose` s2 `compose` s1, apply s3 tv)
+    ELet (PVar x) e1 e2 -> do
+        (s1, t1) <- infer env e1
+        let env' = apply s1 env
+              t' = generalize env' t1
+        (s2, t2) <- infer (env' `extend` (x, t')) e2
+        return (s1 `compose` s2, t2)
+    EInt _ -> return (nullSubst, typeInt)
+    EBool _ -> return (nullSubst, typeBool)
+
+lookupEnv :: TypeEnv -> Name -> Infer (Subst, Ty)
+lookupEnv (TypeEnv env) x = do
+    case Map.lookup x env of
+        Nothing -> throwError $ UnboundVariable (show x)
+        Just s  -> do t <- instantiate s
+                      return (nullSubst, t)
+-}
--- haskell-ilc/src/Type.hs
+++ haskell-ilc/src/Type.hs
@@ -1,9 +1,5 @@
 module Type where
 
-import qualified Data.Map.Strict as Map
-
-import Syntax
-
 newtype TVar = TV String
   deriving (Show, Eq, Ord)
 
@@ -13,38 +9,11 @@ data Ty
     | TArr Ty Ty
     deriving (Show, Eq, Ord)
 
+infixr `TArr`
+
+data Scheme = Forall [TVar] Ty
+    deriving (Show, Eq, Ord)
+
 typeInt, typeBool :: Ty
 typeInt  = TCon "Int"
 typeBool = TCon "Bool"
-
-data Scheme = Forall [TVar] Ty
-
-newtype TypeEnv = TypeEnv (Map.Map Name Scheme)
-
-extend :: TypeEnv -> (Name, Scheme) -> TypeEnv
-extend (TypeEnv env) (x, s) = TypeEnv $ Map.insert x s env
-
-
-
-{-check :: Context -> Type -> Expr -> Either String Type
-check ctx ty expr | ty == (typeOf ctx expr) = Right ty
-                  | otherwise               = Left "type error"
-
-typeOf :: Context -> Expr -> Type
-typeOf ctx (EVar x) = ctx Map.! x
-typeOf _   (EInt _) = TInt
-typeOf _   (EBool _) = TBool
-typeOf _   (EString _) = TString
-typeOf _   (ETag _) = TTag -- ^ Necessary?
--- typeOf ctx (EList []) =
-{-typeOf ctx (EList (e:es)) = 
-  where
-    tyE = typeOf ctx e
-    tyEs = foldl (\ty1 ty2 -> (map (check ctx tyE) es)
-    tyCheck = all (==tyE) $ map (check ctx tyE) es-}
--- typeOf ctx (ESet []) =                      
--- typeOf ctx (ESet (e:es)) =
--- typeOf ctx (ETuple []) =
--- typeOf ctx (ETuple (e:es)) =
-typeOf _ EUnit = TUnit
--- typeOf ctx (EPlus e1 e2) = -}
